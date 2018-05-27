fc5299632deefea65f383ccacfd34b8587259aed
Tanis Newlun <tanishalfelven@users.noreply.github.com>
2018-05-27 01:09:38 -0700

    Dynamically load serverlist data
    This system sucks. Some serverside caching could really speed this up, really sucks right now. Basic functionality is there though, want to add the ability to favorite servers, save that into a cookie so that we can always have favorite servers at the top.

--- src/lib/master-server.js
+++ src/lib/master-server.js
@@ -1,8 +1,6 @@
-
 let MasterServer = {
     _socket : null,
-    init(server_address, onMessage) {
-        this.onMessage = onMessage;
+    init(server_address, serverLoaded, statusLoaded) {
         this._socket = new WebSocket(server_address);
         this._socket.onopen = () => {
             this._socket.send("askforservers#%");
@@ -13,17 +11,42 @@ let MasterServer = {
             let serverData = {};
             if (header === "SN") {
                 const [,address,, port, name, description] = messageData;
-                serverData = {address, port, name, description};
-            } else {
-                const versionType = header === "servercheok" ? "client" : header === "SV" ? "server" : null;
-                if (versionType) {
-                    serverData[versionType] = messageData[0];
-                }
-            }
-            if (Object.keys(serverData).length !== 0) {
-                this.onMessage(serverData);
+                serverData = {address, port, name, description, isStatusLoaded:false, isOnline: null};
+                serverLoaded(serverData);
+                MasterServer.loadOnlineStatus(serverData, statusLoaded);
             }
         };
     },
+    /**
+     * TODO This system would really benefit from some online caching system
+     * Create connection to server and load data, then disconnect so we can display in server list.
+     * @param {object} server An object that represents a server (ip, port, server name, )
+     */
+    loadOnlineStatus(serverData, statusLoaded) {
+        let s = new WebSocket(`ws://${serverData.address}:${serverData.port}`);
+        setTimeout(() => {
+            // prevent infinite spinning, close after 15s
+            serverData = Object.assign(serverData, {isStatusLoaded: true, isOnline: false});
+            s.close();
+        }, 15000);
+        s.onerror = () => {
+            serverData = Object.assign(serverData, {isStatusLoaded: true, isOnline: false});
+            s.close();
+        };
+        s.onopen = () => {
+            s.send("HI#" + navigator.userAgent + "#%");
+            s.send("ID#webAO#2.4.5#%");
+        };
+        s.onmessage = (e) => {
+            const [header, ...messageData] = e.data.split("#");
+            if (header === "PC") {
+                serverData = Object.assign(serverData, {isOnline:true, online:parseInt(messageData[0]), max:parseInt(messageData[1])});
+            }
+            s.close();
+        };
+        s.onclose = () => {
+            statusLoaded(serverData);
+        }
+    }
 };
 export default MasterServer;
--- src/view/server-hub/server-hub.js
+++ src/view/server-hub/server-hub.js
@@ -5,12 +5,23 @@ import MasterServer from "../../lib/master-server";
 let ServerHub = {
     oninit : (vnode) => {
         vnode.state.serverList = [];
-        MasterServer.init("ws://master.aceattorneyonline.com:27016", (serverData) => {
-            if (!serverData["client"] && !serverData["server"]) {
+        MasterServer.init("ws://master.aceattorneyonline.com:27016",
+            // When data for server is received
+            (serverData) => {
                 vnode.state.serverList.push(serverData);
+                m.redraw();
+            },  
+            // When online status is received
+            (serverData) => {
+                for (let i = 0; i < vnode.state.serverList.length; i++) {
+                    if (vnode.state.serverList[i].address === serverData.address && vnode.state.serverList[i].port === serverData.port) {
+                        vnode.state.serverList[i] = serverData;
+                        break;
+                    }
+                }
+                m.redraw();
             }
-            m.redraw();
-        });
+        );
 
         vnode.state.toggleDescription = (e) => {
             e.target.closest(`.${css.server}`).children[1].classList.toggle(css.visible);
@@ -18,15 +29,42 @@ let ServerHub = {
     },
     view : (vnode) => {
         return [m("h2", "Available Connections"),
-            m("div", { class : css.serversContainer}, [vnode.state.serverList && vnode.state.serverList.map((e) => {
-                return m("div", { class : css.server, onclick : vnode.state.toggleDescription }, [
-                    m("div", { class : css.serverData }, [
-                        m("div", { class : css.address }, `${e.address}:${e.port}`),
-                        m("div", { class : css.name}, e.name)
-                    ]),
-                    m("div", { class : css.description }, e.description)
-                ]);
-            })])
+            vnode.state.serverList.length <= 0 ?
+                m("img", { class : css.loadingServersList, src : "./resources/img/spinner.gif" })
+                :
+                m("div", { class : css.serversContainer}, [vnode.state.serverList && Array.from(vnode.state.serverList).sort((a, b) => {
+                    // TODO fix this
+                    // TODO figure out a way for this not to be so ugly
+                    if (a.isOnline !== null && b.isOnline !== null) {
+                        if      (a.isOnline && !b.isOnline) return -1;
+                        else if (!a.isOnline && b.isOnline) return 1;
+                        if (a.isOnline && b.isOnline) {
+                            if (a.online / a.max > b.online / b.max) return -1;
+                            else return 1;
+                        }
+                    } else {
+                        if      (a.isStatusLoaded && !b.isStatusLoaded) return -1;
+                        else if (!a.isStatusLoaded && b.isStatusLoaded) return 1;
+                        else return 0;
+                    }
+                }).map((e) => {
+                    return m("div", { class : css.row }, [
+                        m("div", { class : css.serverStatus }, 
+                            e.isStatusLoaded ?
+                                m("div", { class : e.isOnline ? css.serverOnline : css.serverOffline })
+                                :
+                                m("img", { src : "./resources/img/spinner.gif" })
+                        ),
+                        m("div", { class : css.server, onclick : vnode.state.toggleDescription }, [
+                            m("div", { class : css.serverData }, [
+                                m("div", { class : css.address }, `${e.address}:${e.port}`),
+                                m("div", { class : css.playerCount },e.isStatusLoaded && e.isOnline ? `${e.online} / ${e.max}` : null),
+                                m("div", { class : css.name}, e.name)
+                            ]),
+                            m("div", { class : css.description }, e.description)
+                        ])
+                    ]);
+                })])
         ];
     }
 };
